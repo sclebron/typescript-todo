b4aa9134744292be170856f174a872d3d4f80d87
SLeBron <sophieclebron@gmail.com>
2023-09-11 10:17:58 -0600

    started index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+import { v4 as uuidV4 } from 'uuid'
 
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.querySelector<HTMLFormElement>("#new-task-form")
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+form?.addEventListener("submit", e => {{
+    e.preventDefault()
+
+    if (input?.value == "" || input?.value == null) return 
+
+    const task = {
+        id: uuidV4(),
+        title: input.value,
+        completed: false,
+        createdAt: new Date()
+    }
+}})
